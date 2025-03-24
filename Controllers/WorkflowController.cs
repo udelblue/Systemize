015458b0b0c8a1e7af7fe0f0ffa93d90126854f8
Chris Sommers <cdsommers@gmail.com>
2025-03-23 22:37:11 -0400

    updates

--- Controllers/WorkflowController.cs
+++ Controllers/WorkflowController.cs
@@ -14,6 +14,9 @@ namespace Systemize.Controllers
             _context = context;
         }
 
+        // WORKFLOW
+
+
         // GET: Workflow
         public async Task<IActionResult> Index()
         {
@@ -42,40 +45,6 @@ namespace Systemize.Controllers
 
             return View(workflow);
         }
-
-
-
-
-
-
-            return RedirectToAction(nameof(Details), new { id = workflowId });
-        }
-
-
-        [HttpPost]
-        public async Task<IActionResult> OnPostUploadAsync(List<IFormFile> files)
-        {
-            long size = files.Sum(f => f.Length);
-
-            foreach (var formFile in files)
-            {
-                if (formFile.Length > 0)
-                {
-                    var filePath = Path.GetTempFileName();
-
-                    using (var stream = System.IO.File.Create(filePath))
-                    {
-                        await formFile.CopyToAsync(stream);
-                    }
-                }
-            }
-
-            // Process uploaded files
-            // Don't rely on or trust the FileName property without validation.
-
-            return Ok(new { count = files.Count, size });
-        }
-
 
         // GET: Workflow/Create
         public IActionResult Create()
