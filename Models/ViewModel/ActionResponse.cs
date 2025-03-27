157debfef91d274e7658c2d081ec772800e8c038
Chris Sommers <cdsommers@gmail.com>
2025-03-27 19:13:55 -0400

    updates

---
+++ Models/ViewModel/ActionResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Systemize.Models.ViewModel
+{
+    public class ActionResponse
+    {
+
+        public String ActionType { get; set; }
+        public String? Executor { get; set; }
+        public Dictionary<string, string> Data { get; set; }
+
+    }
+}
---
+++ Services/IProcess.cs
@@ -0,0 +1,11 @@
+﻿using Systemize.Models;
+using Systemize.Models.ViewModel;
+
+
+namespace Systemize.Services
+{
+    public interface IProcess
+    {
+        Workflow Execute(Workflow workflow, ActionResponse response);
+    }
+}
