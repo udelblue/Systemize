6fde299783e2d2e96258befdcfe39ebd34abed36
Chris Sommers <cdsommers@gmail.com>
2025-04-09 20:31:18 -0400

    add libraries for ai

---
+++ Services/IAsyncActionProcess.cs
@@ -0,0 +1,11 @@
+﻿using Systemize.Models;
+using Systemize.Models.ViewModel.Workflow;
+
+
+namespace Systemize.Services
+{
+    public interface IAsyncActionProcess
+    {
+        Task<Workflow> ExecuteAsync(Workflow workflow, ActionResponse response);
+    }
+}
